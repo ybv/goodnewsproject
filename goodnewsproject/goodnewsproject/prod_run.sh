7bf94c2c7839324dea3d7dd08f6584ae0c4a0c3f
ybv <ybv.kris@gmail.com>
2016-04-23 11:59:53 -0700

    change path

--- goodnewsproject/goodnewsproject/prod_run.sh
+++ goodnewsproject/goodnewsproject/prod_run.sh
@@ -4,4 +4,4 @@ SITE_URL='http://104.207.148.127/'
 DB_PASSWORD='123Password'
 DB_NAME='gnpdb'
 DB_USER='postgres'
-ENV=$ENV SITE_URL=$SITE_URL DB_PASSWORD=$DB_PASSWORD DB_USER=$DB_USER /root/goodnewsproject/venv/bin/python /root/goodnewsproject/goodnewsproject/goodnewsproject/manage.py runserver 0.0.0.0:80
+ENV=$ENV SITE_URL=$SITE_URL DB_PASSWORD=$DB_PASSWORD DB_USER=$DB_USER /root/goodnewsproject/venv/bin/python /root/goodnewsproject/goodnewsproject/manage.py runserver 0.0.0.0:80
